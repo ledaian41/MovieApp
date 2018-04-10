891cd6757b1331c5bd4fe074f3a2bd9ddd5452c0
Phan Thanh Loc <phan.thanhloc1705@gmail.com>
2018-04-11 00:16:35 +0700

    update code

--- app/src/main/java/com/example/lean/movieapp/login/LoginActivity.java
+++ app/src/main/java/com/example/lean/movieapp/login/LoginActivity.java
@@ -3,23 +3,30 @@ package com.example.lean.movieapp.login;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
+import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
 import com.example.lean.movieapp.MainActivity;
 import com.example.lean.movieapp.R;
 import com.example.lean.movieapp.common.BaseActivity;
+import com.example.lean.movieapp.common.Utils;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 
 public class LoginActivity extends BaseActivity implements View.OnClickListener, LoginInterface.View {
+    private final String TAG = "LoginActivity";
     private LoginPresenter mLoginPresenter;
     private EditText edtEmail;
     private EditText edtPassword;
@@ -32,10 +39,12 @@ public class LoginActivity extends BaseActivity implements View.OnClickListener,
         initView();
     }
 
+
     private void initView() {
         edtEmail = findViewById(R.id.edtEmail);
         edtPassword = findViewById(R.id.edtPassword);
         findViewById(R.id.btnLogin).setOnClickListener(this);
+        findViewById(R.id.btnLoginGoogle).setOnClickListener(this);
     }
 
     @Override
@@ -44,9 +53,17 @@ public class LoginActivity extends BaseActivity implements View.OnClickListener,
             case R.id.btnLogin:
                 doLoginWithEmailAndPassword();
                 break;
+            case R.id.btnLoginGoogle:
+                doLoginWithGoogleAccount();
+                break;
             default:
                 break;
         }
+    }
+
+    private void doLoginWithGoogleAccount() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, Utils.CODE.LOGIN_GOOGLE_CODE);
     }
 
     private void doLoginWithEmailAndPassword() {
@@ -92,5 +109,47 @@ public class LoginActivity extends BaseActivity implements View.OnClickListener,
     protected void onDestroy() {
         super.onDestroy();
         mLoginPresenter.onDestroy();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK) {
+            switch (requestCode) {
+                case Utils.CODE.LOGIN_GOOGLE_CODE:
+                    Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+                    try {
+                        GoogleSignInAccount account = task.getResult(ApiException.class);
+                        fireBaseAuthWithGoogle(account);
+                    } catch (ApiException e) {
+                        Toast.makeText(this, "Google sign in failed", Toast.LENGTH_SHORT).show();
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+
+    private void fireBaseAuthWithGoogle(GoogleSignInAccount acct) {
+        Log.d(TAG, "firebaseAuthWithGoogle:" + acct.getId());
+
+        AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
+        mFireBaseAuth.signInWithCredential(credential)
+                .addOnFailureListener(this, new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w(TAG, "signInWithCredential:failure", e.getCause());
+                        Toast.makeText(LoginActivity.this, "Authentication Failed", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .addOnSuccessListener(this, new OnSuccessListener<AuthResult>() {
+                    @Override
+                    public void onSuccess(AuthResult authResult) {
+                        if (authResult != null) {
+                            startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                        }
+                    }
+                });
     }
 }
